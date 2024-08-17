b736f4033b9dc1e761e0d8e16db7c41cb01965f1
makmodol1173 <makmodol1173@gmail.com>
2024-08-17 14:24:23 +0600

    New commit

---
+++ JavaScript/profile.js
@@ -0,0 +1,8 @@
+function toggleEdit(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
